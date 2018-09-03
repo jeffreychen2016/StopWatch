7eab962ddcc1a5578f0851f41ac90102e9f2b98b
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-09-03 14:07:47 -0500

    finished online class exerise StopWatch

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -6,7 +6,36 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            var isStarted = false;
+            var watch = new Watch();
+
+            while (!isStarted)
+            {
+                var validInput = false;
+                while (!validInput)
+                {
+                    Console.WriteLine("Please enter START to start the clock...");
+                    if (Console.ReadLine().ToUpper() == "START")
+                    {
+                        watch.start();
+                        isStarted = true;
+                        validInput = true;
+                    }
+                }
+
+                validInput = false;
+                while (!validInput)
+                {
+                    Console.WriteLine("Please enter STOP anytime to stop the clock...");
+                    if (Console.ReadLine().ToUpper() == "STOP")
+                    {
+                        watch.stop();
+                        isStarted = false;
+                        validInput = true;
+                    }
+                }
+            }
         }
     }
 }
---
+++ StopWatch/Watch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Watch
+    {
+        private DateTime _clickInTime;
+        private DateTime _clockOutTime;
+
+        public void start()
+        {
+            _clickInTime = DateTime.Now;
+        }
+
+        public void stop()
+        {
+            _clockOutTime = DateTime.Now;
+
+            getDuration(_clickInTime, _clockOutTime);
+        }
+
+        private void getDuration(DateTime clockInTime, DateTime clockOutTime)
+        {
+            var duration = clockOutTime - clockInTime;
+            Console.WriteLine($"Duration: {duration}");
+        }
+    }
+}
